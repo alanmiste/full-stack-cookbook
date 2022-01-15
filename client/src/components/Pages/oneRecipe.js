6d3a0c5536da0a7991c5cd1ba06bf03a492a08e9
alanmiste <baranalaan@gmail.com>
2022-01-16 00:16:32 +0100

    show ingredient and preparation

--- client/src/components/Pages/oneRecipe.js
+++ client/src/components/Pages/oneRecipe.js
@@ -1,35 +1,46 @@
 import react from 'react'
-import {useParams} from 'react-router-dom'
+import { useParams } from 'react-router-dom'
 import { useContext, useEffect, useState } from 'react'
-import {RecipeContext} from '../Context/context'
+import { RecipeContext } from '../Context/context'
 
-export default function OneRecipe(){
+export default function OneRecipe() {
 
-    const {id} = useParams() //to get the id from the url
+    const { id } = useParams() //to get the id from the url
     const { recipes } = useContext(RecipeContext) //get data from context.js
 
     const [recipe, setRecipe] = useState({}) //to save recipes in it locally depending on id
 
-    useEffect(()=>{
+    useEffect(() => {
         const idx = recipes.findIndex(item => item._id === id) /* Comparison of item._id which is 
         the base id for the recipe that comes from the database via context, 
         and the id that comes from the link via url, and save the index of recipe resulting from 
         the comparison in idx. */
         //now idx = 1 or 2 or 3 or...., depends on the comparison result
         console.log('useEffect ', idx)
-        setRecipe({...recipes[idx]})
-    },[])
-    console.log('this is recipe',recipe)
+        setRecipe({ ...recipes[idx] })
+    }, [])
+    console.log('this is recipe', recipe)
 
-    return(
+    return (
         <div>
-            One Recipe
-            <div>
-            <div>{
-                recipe.title
+            <h4>{recipe.title}</h4>
+            <div><h5>Ingredient</h5>
+                {
+                recipe.ingredient?.map(
+                item=><ul>
+                    <li>{item}</li>
+                </ul>)
                 }
             </div>
-        </div>
+            <div><h5>Preparation</h5>
+                {
+                    recipe.preparation?.map(
+                        item => <ul>
+                            <li> {item} </li>
+                        </ul>
+                        )
+                }
+            </div>
         </div>
     )
 }
