cd8d68b28e93d17ac88c2f4b1163d1a80b8e1e4a
alanmiste <baranalaan@gmail.com>
2022-01-20 14:10:50 +0100

    add addRecipe fields

--- client/src/components/Pages/addRecipe.js
+++ client/src/components/Pages/addRecipe.js
@@ -15,22 +15,23 @@ export default function AddRecipe() {
 
     //     console.log('data= ',data)
     // }
-    
-    
-    const addRecipe = async ()=>{
+
+
+    const addRecipe = async () => {
         console.log('Hello from addRecipes')
 
         // "_id":{"$oid":"61db37f9ac938a7765f06767"}
         const recipe = {
-            "title":"Test recipe",
-            "ingredient":["4 tbsp passata",
+            "title": "Test recipe",
+            "ingredient": ["4 tbsp passata",
                 "1 garlic clove , crushed"],
-            "preparation":["Heat the oven to 220C.",
+            "preparation": ["Heat the oven to 220C.",
                 "Mix the passata, garlic, olive"],
-            "published":false,
-            "category":"pizza",
-            "cooktime":"18 min",
-            "description":"This is an extremely easy."}
+            "published": false,
+            "category": "pizza",
+            "cooktime": "18 min",
+            "description": "This is an extremely easy."
+        }
 
         const response = await axios.post('/recipes/add', recipe)
 
@@ -39,7 +40,78 @@ export default function AddRecipe() {
         window.location.reload(false); //to refresh the page after the recipe has been added
     }
 
+    //---------------------------------------------------------
 
+    const [text, setText] = useState();
+
+    const handleChange = (e) => {
+
+        e.preventDefault()
+        console.log(e.target.value)
+        setText(e.target.value)
+    }
+
+    const [RName, setRName] = useState('');
+    const [RDescription, setRDescription] = useState('');
+    const [RIngredients2, setRIngredients2] = useState([]);
+    const [RRreparation4, setRPreparation4] = useState([]);
+
+    const [addPost, setAddPost] = useState(
+        {
+            'title': '',
+            'ingredient': [],
+            'preparation': [],
+            'published': false,
+            'category': '',
+            'cooktime': '',
+            'description': ''
+        }
+    );
+
+
+    function setName() {
+        setRName(text);
+        document.getElementById('nameInput').value = '';
+        // console.log('from setName',RName);
+    }
+
+    function setDescription() {
+        setRDescription(text);
+        document.getElementById('descriptionInput').value = '';
+        // console.log('from setDescription ',RDescription)
+    }
+
+    // useEffect(() => setIngredients(), []);
+    function setIngredients() {
+        setRIngredients2(arr => [...arr, `${text}`]);
+        document.getElementById('ingredientInput').value = '';
+        document.getElementById("ingredientInput").focus();
+        // console.log('from setIngredients',RIngredients2)
+    }
+
+    function setPreparation() {
+        setRPreparation4(arr => [...arr, `${text}`]);
+        document.getElementById('preparationInput').value = '';
+        document.getElementById("preparationInput").focus();
+        // console.log('from setPreparation',RRreparation4)
+    }
+
+    const [addCategory, setAddCategory] = useState('Random');
+    function handleCategory(e) {
+        setAddCategory(e.target.value);
+        console.log(addCategory);
+    }
+
+
+
+
+
+    function handleClick() {
+
+        console.log('handleclick')
+        console.log('this is category ', addCategory)
+
+    }
 
 
 
@@ -47,6 +119,62 @@ export default function AddRecipe() {
         <div>
             add Recipe
             <button onClick={addRecipe}>add Recipe</button>
+
+            <div id="form">
+                <h2 id="sBigger">Add New Recipe</h2>
+
+                <div className="forFlexing">
+                    <div className='inputForum'>
+
+                        <div className='inputF'>
+                            <label>Recipe Name: </label>
+                            <input id='nameInput' type="text" onChange={handleChange}></input>
+                            <button onClick={setName}>add Recipe name</button>
+                        </div>
+                        <div className='inputF'>
+                            <label>Choose Category: </label>
+                            <select
+                                defaultValue={addCategory}
+                                onChange={handleCategory}
+                                className='inputF'
+                            >
+                                <option value="pizza">Pizza</option>
+                                <option value="dessert">Dessert</option>
+                                <option value="noodle">Noodle</option>
+                                <option value="random">Random</option>
+                            </select>
+                        </div>
+                        <div className='inputF'>
+                            <label>Description: </label>
+                            <textarea id='descriptionInput' name="message" rows="10" cols="30" onChange={handleChange}></textarea>
+                            <button onClick={setDescription}>add Description</button>
+                        </div>
+                        <div className='inputF'>
+                            <label>Ingredients: </label>
+                            <input id='ingredientInput' type="text" onChange={handleChange}></input>
+                            <button onClick={setIngredients}>add one</button>
+                            <div id='ingShow'></div>
+                        </div>
+                        <div className='inputF'>
+                            <label>Preparation: </label>
+                            <input id='preparationInput' type="text" onChange={handleChange}></input>
+                            <button onClick={setPreparation}>add one</button>
+                        </div>
+                        <div>
+                            <button className='addRecipeBtn' onClick={handleClick}>Add Recipe</button>
+                        </div>
+                    </div>
+                    <div className='Preview'>
+                        <h3>Preview</h3>
+                        <div className="preview">
+                            <p>{RName}</p>
+                            <p>{addCategory}</p>
+                            <p>{RDescription}</p>
+                            {/* <ul>{List(RIngredients2)}</ul>
+                        <ol>{List(RRreparation4)}</ol> */}
+                        </div></div>
+                </div>
+            </div>
         </div>
 
     )
