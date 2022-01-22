825bcfff2852bd938bcf2daed926050e3dcb93fe
alanmiste <baranalaan@gmail.com>
2022-01-22 17:23:13 +0100

    fix heroku staf

--- server/server.js
+++ server/server.js
@@ -38,5 +38,16 @@ connectDB()
  app.use('/recipes', require('./api/recipes'));
  // 4. listen to some port
  
+ app.use(express.static('client/build'));
+
+if( process.env.NODE_ENV === 'production' ) {
+
+    const path = require('path');
+
+    app.get('/*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, '../client', 'build', 'index.html'));
+
+})
+}
  const port = process.env.PORT || 8008;
  app.listen(port, () => console.log('Server is UP and Running at port: ' + port))
